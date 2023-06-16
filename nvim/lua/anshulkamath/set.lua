e3d67a52a24aee3424fc565f548ff41a62de3bbe
Anshul Kamath <anshulkam@gmail.com>
2023-06-16 16:11:35 -0400

    update: added settings to vim experience

--- nvim/lua/anshulkamath/set.lua
+++ nvim/lua/anshulkamath/set.lua
@@ -11,3 +11,24 @@ vim.opt.smartindent = true
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
+-- turn off vim backups and default to undotree
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile = true
+
+-- turn off search highlighting
+vim.opt.hlsearch = false
+
+-- turn on incremental search
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+
+vim.opt.scrolloff = 8
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
