9ded443835d1545a292a8e42836fbf2a94ce7b28
Anshul Kamath <anshulkam@gmail.com>
2023-07-11 15:50:01 -0400

    add: setup script for symlink migration

---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+wd=$(pwd)
+
+ln -sf "$wd/alacritty" ~/.config
+ln -sf "$wd/nvim" ~/.config
+ln -sf "$wd/tmux" ~/.config
+ln -sf "$wd/bin" ~/.config
+ln -sf "~/.gitconfig" ~
+ln -sf "~/.gitignore-global" ~
