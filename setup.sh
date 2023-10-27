ae0ad35852b987f3034de0040a968e6fa2f2ac80
Anshul Kamath <anshul.kamath@quantcast.com>
2023-10-27 09:47:50 -0700

    Rename: .gitignore-global -> gitignore-global
    Renamed gitignore file and added symlink to setup script.

--- setup.sh
+++ setup.sh
@@ -7,6 +7,10 @@ ln -sf "$wd/tmux" ~/.config
 ln -sf "$wd/bin" ~/.config
 ln -sf "$wd/kitty" ~/.config
 
+mkdir -p ~/.config/git
+ln -sf "$wd/gitignore-global" ~/.config/git/ignore
+git config --global core.excludesFile ~/.config/git/ignore
+
 if [[ ! -d /usr/local/bin ]]; then
   mkdir -p /usr/local/bin
   echo "Created /usr/local/bin. Make sure that it is in your path"
