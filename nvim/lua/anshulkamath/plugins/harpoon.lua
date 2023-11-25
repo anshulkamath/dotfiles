2757c94d3b9d8a3da5e2973e0e5d46d7bbf7b5ce
Anshul Kamath <anshul.kamath@quantcast.com>
2023-11-25 13:16:35 -0500

    Misc: Add and update several packages
    This commit adds the following packages:
     - which-key (provides hints for keymaps)
     - vim-signature (marks and marker utilities)
     - treesitter-context (floating context for long functions)
    
    Also adds some updates for harpoon keymaps and more intuitive nvim-cmp
    bindings.

--- nvim/lua/anshulkamath/plugins/harpoon.lua
+++ nvim/lua/anshulkamath/plugins/harpoon.lua
@@ -11,7 +11,12 @@ return {
 
 		keymap.set("n", "<leader>eh", ui.toggle_quick_menu, { desc = "Toggle harpoon quick menu" })
 		keymap.set("n", "<leader>hm", mark.add_file, { desc = "Mark file with harpoon" })
+		keymap.set("n", "<leader>hc", mark.clear_all, { desc = "Clear all files from harpoon" })
 		keymap.set("n", "]h", ui.nav_next, { desc = "Go to next harpoon mark" })
 		keymap.set("n", "[h", ui.nav_prev, { desc = "Go to previous harpoon mark" })
+
+		keymap.set("n", "<leader>hf", function()
+			ui.nav_file(tonumber(vim.fn.input("Harpoon buffer:")))
+		end)
 	end,
 }
