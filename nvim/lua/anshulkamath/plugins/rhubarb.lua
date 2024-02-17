72b43783f09765fc169e6345a985dbe20a9df3d4
anshulkamath <anshulkam@gmail.com>
2024-02-16 20:11:36 -0800

    Nvim: Update aerial. Add rhubarb, indent-blankline

--- nvim/lua/anshulkamath/plugins/rhubarb.lua
+++ nvim/lua/anshulkamath/plugins/rhubarb.lua
@@ -1,3 +1,10 @@
 return {
 	"tpope/vim-rhubarb",
+	config = function()
+		local keymap = vim.keymap
+		keymap.set("n", "<leader>gb", "<cmd>GBrowse<cr>", { desc = "Open file in remote repo" })
+		keymap.set("v", "<leader>gb", "<cmd><'>'GBrowse<cr>", { desc = "Open lines in remote repo" })
+		keymap.set("n", "<leader>gy", "<cmd>GBrowse!<cr>", { desc = "Copy remote link to clipboard" })
+		keymap.set("v", "<leader>gy", "<cmd>'<,'>GBrowse!<cr>", { desc = "Copy remote link to clipboard" })
+	end,
 }
